422d5c0592d27f68e803f90b51465b9d35871e02
wangrui <276411794@qq.com>
2020-12-03 23:12:45 +0800

    编写 springboot 的简单 dome，方便学习 jenkins。

---
+++ src/main/java/com/spring/demo/springbootdemo/control/GeiApi.java
@@ -0,0 +1,16 @@
+package com.spring.demo.springbootdemo.control;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping()
+public class GeiApi {
+
+    @GetMapping("get")
+    @ResponseBody
+    public String getInfo() {
+        return "线路畅通";
+    }
+}
